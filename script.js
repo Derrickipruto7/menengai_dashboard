228a7141e8062ac78dc5c7b16eba9136ec7efabe
Derrickipruto7 <169073429+Derrickipruto7@users.noreply.github.com>
2026-09-02 09:43:07 +0300

    Increase road layer weight from 0.2 to 0.8

--- script.js
+++ script.js
@@ -64,7 +64,7 @@ Promise.all([
  roadsLayer = L.geoJSON(roads, {
   style: {
     color: '#FFB6C1',   // Light pink
-    weight: 0.2,
+    weight: 0.8,
     fillOpacity: 0.04,
     fillColor: '#FFB6C1'
   }
